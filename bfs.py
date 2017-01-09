a2d4188878f8b13cf44c32a45b3c1d33b78327d5
kevin <kevin@eduroam-65-190.loria.fr>
2017-01-09 14:58:28 +0100

    Class graph2vec to ease the use of the methods

--- bfs.py
+++ bfs.py
@@ -1,40 +1,46 @@
 import numpy as np
-from scipy.sparse import coo_matrix
 
+class graph2vec():
+    def __init__(self):
+        self.graph_matrix = None
+        self.vectors = None
+        self.individuals = None
 
-def get_paths_source(graph, source, individuals):
-    "From a source node and given a list of individual nodes, returns the depths of the paths between the source node and all other attribute nodes"
-    queue = [(source, [source])]
-    attributes = set(graph.keys()) - set(individuals)
-    output = {attribute:np.Inf for attribute in attributes}
+    def fit(self, graph, individuals):
+        self.graph_matrix = graph
+        self.individuals = individuals
 
-    while queue:
-        vertex, path = queue.pop(0)
-        for next in set(graph[vertex]) - set(path):
-            if (next not in individuals):
-                queue.append((next,path+[next]))
-                depth = len(path)
-                if (output[next] == np.Inf):
-                    output[next] = depth
-                elif (output[next] is not 1):
-                    output[next] -= float(1)/depth
+    def get_paths_source(self,source):
+        "From a source node and given a list of individual nodes, returns the depths of the paths between the source node and all other attribute nodes"
+        individuals = self.individuals
+        graph = self.graph_matrix
+        queue = [(source, [source])]
+        attributes = set(graph.keys()) - set(individuals)
+        output = {attribute:np.Inf for attribute in attributes}
+
+        while queue:
+            vertex, path = queue.pop(0)
+            for next in set(graph[vertex]) - set(path):
+                if (next not in individuals):
+                    queue.append((next,path+[next]))
+                    depth = len(path)
+                    if (output[next] == np.Inf):
+                        output[next] = depth
+                    elif (output[next] is not 1):
+                        output[next] -= float(1)/depth
  
-    return output
+        return output
 
-def bfs_level(graph, individuals):
-    rows = []
-    cols = []
-    data = []
-    for individual in individuals:
-        bfs_local = get_paths_source(graph,individual,individuals)
-        cols_local = bfs_local.keys()
-        data_local = get_similarity(bfs_local.values())
-        rows_local = [individual for x in range(len(data_local))]
-        rows.extend(rows_local)
-        cols.extend(cols_local)
-        data.extend(data_local)
-    output = coo_matrix((data,(rows,cols)))
-    return output
+    def get_vectors(self):
+        "Returns vectors representing each individual node in a tuple in the form (vector, basis)"
+        individuals = self.individuals
+        output = {individual:[] for individual in individuals}
+        for individual in individuals:
+            bfs_local = self.get_paths_source(individual)
+            cols_local = bfs_local.keys()
+            data_local = get_similarity(bfs_local.values())
+            output[individual] = (data_local,cols_local)
+        return output
 
 def dict_to_list(dict):
     return ([sub_dict[1] for sub_dict in dict.items()],dict.keys())
@@ -58,6 +64,8 @@ def generate_graphs(graph_name):
 graph = generate_graphs("one")
 graph_adj = graph[0]
 graph_individuals = graph[1]
-output = bfs_level(graph_adj,graph_individuals);
+g2v = graph2vec()
+g2v.fit(graph_adj,graph_individuals)
+output = g2v.get_vectors()
 print(output)
 
